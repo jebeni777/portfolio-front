5f26f6357d7bfcd257d0b5f973fee22ce81463cc
Becky <beckymccarthy777@gmail.com>
2019-12-17 12:46:06 -0800

    update after develop

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,11 +1,11 @@
 var plugins = [{
-      plugin: require('/Users/unloop/myPortfolioSite/gatsby-starter-dimension/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
+      plugin: require('/Users/unloop/portfolio-front/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
     },{
-      plugin: require('/Users/unloop/myPortfolioSite/gatsby-starter-dimension/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
+      plugin: require('/Users/unloop/portfolio-front/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
       options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/favicon_16x16.png"},
     },{
-      plugin: require('/Users/unloop/myPortfolioSite/gatsby-starter-dimension/node_modules/gatsby-plugin-offline/gatsby-ssr'),
+      plugin: require('/Users/unloop/portfolio-front/node_modules/gatsby-plugin-offline/gatsby-ssr'),
       options: {"plugins":[]},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,9 +5,9 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/unloop/myPortfolioSite/gatsby-starter-dimension/.cache/dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("/Users/unloop/myPortfolioSite/gatsby-starter-dimension/src/pages/404.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/unloop/myPortfolioSite/gatsby-starter-dimension/src/pages/index.js"))),
-  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/unloop/myPortfolioSite/gatsby-starter-dimension/src/pages/page-2.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/unloop/portfolio-front/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/unloop/portfolio-front/src/pages/404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/unloop/portfolio-front/src/pages/index.js"))),
+  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/unloop/portfolio-front/src/pages/page-2.js")))
 }
 
