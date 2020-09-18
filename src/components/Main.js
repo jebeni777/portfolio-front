d280a58f8142848cf825594e5b8c9a3bc79fc390
Becky McCarthy <beckymccarthy777@gmail.com>
2020-09-18 10:59:31 -0700

    fixed spacing

--- src/components/Main.js
+++ src/components/Main.js
@@ -99,7 +99,7 @@ class Main extends React.Component {
             I engineered a React.js progressive web application called <a href="https://master.djb4a0m29g84r.amplifyapp.com/">Comfort Foods</a> to provide actionable dietary information for seniors. This was done by engaging in user research to identify the best experience for the baby boomer generation. The application delivers a creative dietary plan/recommendation list for adding new ingredients to your staple dishes, based on your health needs. This was accomplished with persisted application content in a GraphQL-like database, using Sanity.io. Then deployed the application to the internet using AWS Cloudfront and Amplify.
           </p>
           <p>
-            Here is an ecommerce site for created for<a href="https://woollymammothshoes.com/">Wooly Mammoth Shoes</a>.
+            Here is an ecommerce site for created for <a href="https://woollymammothshoes.com/">Wooly Mammoth Shoes</a>.
           </p>
           {close}
           <span>
